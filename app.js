1642b9bf2145e7e4f6de30f8d6bb89fb7997c678
jponichter <ponichterjot@gmail.com>
2019-10-30 03:15:56 +0100

    Smooth scroll added

--- app.js
+++ app.js
@@ -1,46 +1,58 @@
-
 $("#btn-right").click(() => {
   // anime({
   //     targets: '#coaches',
   //     translateX: -screen.width
   // });
 
-  
-  let fleetHeight = $('#fleet-page').height();
-  console.log(fleetHeight);
-  $('#coaches').height(fleetHeight);
+  if (window.matchMedia("(min-width: 769px)").matches) {
+    let fleetHeight = $("#fleet-page").height();
+    console.log(fleetHeight);
+    $("#coaches").height(fleetHeight);
 
-  let tl = anime.timeline({
-    easing: "easeOutExpo",
-    duration: 750
-  });
+    let tl = anime.timeline({
+      easing: "easeOutExpo",
+      duration: 750
+    });
 
-  tl
-  .add({
-    targets: ".photo",
-    opacity: 0
-  })
-  .add({
-    targets: "#coaches",
-    translateX: -document.body.clientWidth
-  });
+    tl.add({
+      targets: ".photo",
+      opacity: 0
+    }).add({
+      targets: "#coaches",
+      translateX: -document.body.clientWidth
+    });
+  }else{
 
+  }
 });
 
 $("#btn-right-close").click(() => {
+  let tl = anime.timeline({
+    easing: "easeInExpo",
+    duration: 750
+  });
 
-    let tl = anime.timeline({
-        easing: "easeInExpo",
-        duration: 750
-      });
-    
-      tl
-      .add({
-        targets: "#coaches",
-        translateX: document.body.clientWidth
-      })
-      .add({
-        targets: ".photo",
-        opacity: 1
-      });
+  tl.add({
+    targets: "#coaches",
+    translateX: document.body.clientWidth
+  }).add({
+    targets: ".photo",
+    opacity: 1
+  });
+});
+
+//Smooth scrolling
+$(document).ready(function () {
+  $('a[href^="#"]').on('click', function (e) {
+      e.preventDefault();
+
+      let navHeight = $("nav").height();
+
+      var target = this.hash,
+          $target = $(target);
+
+      $('html, body').stop().animate({
+          'scrollTop': $target.offset().top - navHeight
+      }, 900, 'swing');
+  });
 });
